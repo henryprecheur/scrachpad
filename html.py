fb151f231822edbfd6dd5df166b560a10db7e500
Henry Precheur <henry@precheur.org>
2012-02-22 11:58:03 -0800

    scratchpad edit-start: 2012-02-22T11:51:36-08:00

--- html.py
+++ html.py
@@ -1,26 +1,35 @@
 import sys
 
-class Post(dict):
+class Post(object):
     def __init__(self):
         self._header = True
-        self._body = list()
+        self._lines = list()
 
     def feed(self, line):
         if not line or line == '\f\n':
+            self._lines.append('</pre>\n')
             return False
 
         # Poor's man state management
         if self._header:
             if line == '\n':
                 self._header = False
+                self._lines.append('\n<pre>')
             else:
                 if ':' not in line:
                     raise ValueError("Not a valid header: {!r}".format(line))
 
                 name, value = line.split(':', 1)
-                self[name] = value.rstrip('\n')
+                value = value.rstrip('\n')
+
+                if name == 'time':
+                    self._lines.append(
+                        "<time datetime='{}' pubdate>{}</time>\n".\
+                        format(value, value))
+                elif name == 'tags':
+                    self._lines.append('<p>{}</p>\n'.format(value))
         else:
-            self._body.append(line)
+            self._lines.append(line)
 
         return True
 
@@ -36,6 +45,5 @@ if __name__ == '__main__':
     print len(posts)
 
     for p in reversed(posts):
-        print p['tags'], p['time']
-        for l in p._body:
+        for l in p._lines:
             sys.stdout.write(l)
