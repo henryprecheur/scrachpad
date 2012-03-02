f2d2381dd3a07b6788417179e4fe3b6e44b95df9
Henry Precheur <henry@precheur.org>
2012-03-01 17:08:48 -0800

    Add atom generator

---
+++ atom.py
@@ -0,0 +1,36 @@
+import sys
+from string import Template
+
+import common
+
+url = 'http://henry.precheur.org/scratchpad'
+
+if __name__ == '__main__':
+    posts = tuple(reversed(common.posts(sys.stdin)))
+
+    updated, _ = posts[0]
+    sys.stdout.write(Template('''\
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+
+<id>${url}</id>
+<title>Scratchpad</title>
+<updated>${time}</updated>
+<link rel="self" href="$url/atom.xml" />
+<link rel="self" href="$url/" />\n
+''').safe_substitute(url=url, time=updated))
+
+    for id_, body in posts:
+        sys.stdout.write(Template('''\
+<entry>
+    <id>${url}/#${ref}</id>
+    <link href='${url}/#${ref}' />
+    <title>${ref}</title>
+    <updated>${ref}</updated>
+    <content type="xhtml">
+       <pre>''').safe_substitute(url=url, ref=id_))
+        for l in body:
+            sys.stdout.write(common.format(l, id_))
+        sys.stdout.write('</pre>\n    </content>\n</entry>\n')
+
+    sys.stdout.write('</feed>')
