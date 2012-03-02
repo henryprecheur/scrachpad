ca968503a1e1887dbcc87ada264214c625d09138
Henry Precheur <henry@precheur.org>
2012-03-01 17:03:38 -0800

    Add link to article

--- html.py
+++ html.py
@@ -6,15 +6,18 @@ if __name__ == '__main__':
 <title>Scratch pad</title>
 <link rel='stylesheet' href='https://raw.github.com/necolas/normalize.css/master/normalize.css'>
 <style>
-    html { font-family: sans-serif; }
+    html { font: normal medium sans-serif; }
     body, header, footer, article { margin: 2em; }
     article > pre { width: 48em; white-space: pre-wrap; }
 </style>
 <header>This is my strachpad, where I learn and make mistakes.</header>\n''')
 
     for id_, body in reversed(common.posts(sys.stdin)):
-        sys.stdout.write('<article>\n<time datetime=%s pubdate>%s</time>\n'
-                         '<pre>' % (id_, id_))
+        sys.stdout.write('<article id=%s>\n'
+                         '<a href=#%s>'
+                         '<time datetime=%s pubdate>%s</time>'
+                         '</a>\n'
+                         '<pre>' % (id_, id_, id_, id_))
         for l in body:
             sys.stdout.write(common.format(l, id_))
         sys.stdout.write('</pre>\n</article>\n')
