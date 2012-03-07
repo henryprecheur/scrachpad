3a47b32ede07fbf3b2216b0597609caf0fcc1ad3
Henry Precheur <henry@precheur.org>
2012-03-06 22:19:06 -0800

    Add tracking from Google analytics because I'm a bad person

--- html.py
+++ html.py
@@ -24,4 +24,18 @@ if __name__ == '__main__':
     sys.stdout.write('''\n<hr>
 <footer>Contact me:
 <a href='mailto:Henry Precheur <henry@precheur.org>'>Henry \
-Pr&ecirc;cheur</a></footer>''')
+Pr&ecirc;cheur</a></footer>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20945988-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>''')
