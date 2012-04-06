1227d0ec6a1ff461d5907e46c2a8a87c63150b25
Henry Precheur <henry@precheur.org>
2012-04-05 17:43:31 -0700

    Add WIP

---
+++ stats.py
@@ -0,0 +1,56 @@
+from common import posts
+from datetime import date
+
+def date_from_id(i):
+    i = i[:len('2012-03-28')]
+    year, month, day = i.split('-')
+    return date(int(year), int(month), int(day))
+
+def sort_by_week(posts):
+    weeks = list()
+
+    prev = None
+    bodies = list()
+    for id_, body in posts:
+        d = date_from_id(id_)
+        isoweek = '{0:04}-{1:02}'.format(*d.isocalendar())
+
+        if isoweek == prev:
+            bodies.append(body)
+        else:
+            bodies = [body]
+            weeks.append((isoweek, bodies))
+            prev = isoweek
+    return weeks
+
+def stats(texts):
+    return (
+        sum(len(t) for t in texts),
+        sum(len(t.split()) for t in texts)
+    )
+
+def html(posts, output):
+    output.write('''<!DOCTYPE html>
+<title>Scratchpad stats</title>
+<style>
+    li { border-color: black; border-style: solid }
+</style>
+<ul>
+''')
+    for week, texts in sort_by_week(posts):
+        chars, words = stats(texts)
+        style = 'border-width: 0 {}px 0 {}px'
+        output.write('  <li style="{style}">{week}</li>\n'.\
+                     format(style=style.format(chars // 10, words), week=week))
+    output.write('</table>\n')
+
+def console(posts):
+    for week, texts in sort_by_week(p):
+        print '{}, {}, {}'.format(week, *stats(texts))
+
+if __name__ == '__main__':
+    from sys import stdin, stdout
+
+    p = posts(stdin)
+
+    html(p, stdout)
