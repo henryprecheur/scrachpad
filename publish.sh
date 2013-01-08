de7a9a37bfb46a4cd08d94e9ff49594ed114c7dc
Henry Precheur <henry@precheur.org>
2013-01-08 15:42:15 +0100

    publish.sh so that rc isn't needed anymore

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# cd /var/www/henry.precheur.org/scratchpad/
+
+readonly dir=.log
+test -d $dir || mkdir $dir
+
+tmp=$(date +%FT%T%z | sed 's/\([+-][01][0-9]\)\([0-9][0-9]\)$/\1:\2/')
+( cat; echo  ) | tee -a log $dir/$tmp
+
+if test -x ./python
+then
+    python=./python
+else
+    python=python
+fi
+python=.env/bin/python
+
+$python html.py < log > index.html
+$python atom.py < log > feed.atom
