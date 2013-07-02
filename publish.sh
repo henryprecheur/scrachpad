0a4ca0bdc786a6ed42a7269b0d7794ea2b0e8627
Henry Precheur <henry@precheur.org>
2013-07-02 17:46:55 -0400

    Always use .env/bin/python

--- publish.sh
+++ publish.sh
@@ -5,15 +5,10 @@ cd /var/www/henry.precheur.org/scratchpad/
 readonly dir=.log
 test -d $dir || mkdir $dir
 
-tmp=$(date +%FT%T%z | sed 's/\([+-][01][0-9]\)\([0-9][0-9]\)$/\1:\2/')
-{ cat; echo  } | tee -a log $dir/$tmp
+{
+    cat; echo 
+} | tee -a log $dir/$(date +%s)
 
-if test -x ./python
-then
-    python=./python
-else
-    python=python
-fi
 python=.env/bin/python
 
 $python html.py < log > index.html
