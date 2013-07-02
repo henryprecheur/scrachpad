9131daf56f9c62698fed633f13cdb15cb713fcb2
Henry Precheur <henry@precheur.org>
2013-07-02 14:48:42 -0700

    XHTMLSerializer was removed. Try to generate valid xhtml w/o it

--- xmlize.py
+++ xmlize.py
@@ -2,7 +2,7 @@ import io
 import xml.etree
 import html5lib
 
-def xmlize(input_, encoding=None, output=None):
+def xmlize(input_, output=None):
     '''
     Convert HTML into XHTML.
 
@@ -23,19 +23,22 @@ def xmlize(input_, encoding=None, output=None):
         >>> xmlize(input)
         u'<p>Hello<br />World</p>'
     '''
-    encoding = encoding or 'utf_8'
+    assert isinstance(input_, unicode)
     doc = html5lib.parseFragment(input_,
-                                 encoding=encoding,
                                  treebuilder='etree',
                                  namespaceHTMLElements=False)
 
     walker = html5lib.treewalkers.getTreeWalker('etree', xml.etree.ElementTree)
-    s = html5lib.serializer.XHTMLSerializer()
+    s = html5lib.serializer.HTMLSerializer(
+        omit_optional_tags=False,
+        minimize_boolean_attributes=False,
+        use_trailing_solidus=True,
+        quote_attr_values=True)
 
     stream = s.serialize(walker(doc), encoding='utf-8')
     if output:
         for text in stream:
             output.write(text)
     else:
-        return u''.join(unicode(x, encoding) for x in stream)
+        return u''.join(unicode(x, 'utf8') for x in stream)
     stdout.write(xmlize(stdin))
