c9c14f13a2cf85bcadc6ee91ef71d8a228e859d4
Henry Precheur <henry@precheur.org>
2013-08-05 09:09:52 -0700

    Reswitch to normalize.css & dynamic font size

--- html.py
+++ html.py
@@ -7,32 +7,48 @@ if __name__ == '__main__':
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width">
     <title>Scratch pad</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/pure/0.2.1/pure-min.css'>
-    <link
-     href='//fonts.googleapis.com/css?family=Source+Code+Pro:400,700|Open+Sans'
-     rel='stylesheet'>
+    <link rel='stylesheet'
+     href='//cdnjs.cloudflare.com/ajax/libs/normalize/2.1.0/normalize.css'>
+    <link rel='stylesheet'
+     href='//fonts.googleapis.com/css?family=Source+Code+Pro:400,700|Open+Sans'>
     <style>
         body {
-                margin: 0 auto;
-                max-width: 50rem;
-                font-family: 'Open Sans', sans-serif;
-                line-height: 140%;
+            margin: 0 auto;
+            max-width: 50rem;
+            font-family: 'Open Sans', sans-serif;
+            line-height: 140%;
         }
         p { line-height: 150% }
-        code, kbd, pre, samp { font-family: 'Source Code Pro', monospace , serif; }
+        code, kbd, pre, samp {
+            font-family: 'Source Code Pro', monospace , serif;
+        }
         pre {
-                background-color: silver;
-                border-top: 1px solid grey;
-                border-bottom: 1px solid grey;
-                padding: 0.5em;
+            background-color: silver;
+            border-top: 1px solid grey;
+            border-bottom: 1px solid grey;
+            padding: 0.5em;
         }
         header, footer { padding: 0.5em; margin: 0.5em }
         article { padding: 1.2em; border-bottom: 1px solid silver }
         a { text-decoration: none; color: #3F72D8; border-bottom: 1px dotted }
         a:hover { border-bottom: 1px solid }
+
+        @media all {html {font-size: 24px;}}
+        @media all and (max-width:1000px){html {font-size: 24px;}}
+        @media all and (max-width:960px){html {font-size: 23px;}}
+        @media all and (max-width:920px){html {font-size: 22px;}}
+        @media all and (max-width:880px){html {font-size: 21px;}}
+        @media all and (max-width:840px){html {font-size: 20px;}}
+        @media all and (max-width:800px){html {font-size: 19px;}}
+        @media all and (max-width:760px){html {font-size: 18px;}}
+        @media all and (max-width:720px){html {font-size: 17px;}}
+        @media all and (max-width:680px){html {font-size: 16px;}}
+        @media all and (max-width:640px){html {font-size: 15px;}}
+        @media all and (max-width:600px){html {font-size: 14px;}}
+        @media all and (max-width:560px){html {font-size: 13px;}}
+        @media all and (max-width:520px){html {font-size: 12px;}}
     </style>
 </head>
 <body class='pure'>
