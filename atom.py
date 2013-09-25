2d3d5ab3f6bc238ed5164ef4991d075350af6d96
Henry Precheur <henry@precheur.org>
2013-09-24 22:30:17 -0700

    tweak atom

--- atom.py
+++ atom.py
@@ -1,5 +1,4 @@
 import sys
-from string import Template
 
 from markdown2 import markdown
 
@@ -12,32 +11,33 @@ if __name__ == '__main__':
     posts = tuple(reversed(common.posts(sys.stdin)))
 
     updated, _ = posts[0]
-    sys.stdout.write(Template('''\
+    out = sys.stdout.write
+    out('''\
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
 
-<id>${url}</id>
+<id>{url}</id>
 <title>Scratchpad</title>
-<updated>${time}</updated>
-<link rel="self" href="$url/feed.atom" />
-<link rel="alternate" type="text/html" href="$url/" />\n
-''').safe_substitute(url=url, time=updated))
+<updated>{time}</updated>
+<link rel="self" href="{url}/feed.atom" />
+<link rel="alternate" type="text/html" href="{url}/" />
+'''.format(url=url, time=updated))
 
     for id_, body in posts:
-        sys.stdout.write(Template('''\
+        out('''\
 <entry>
-  <id>${url}/#${ref}</id>
-  <link href='${url}/#${ref}' />
-  <title>${ref}</title>
-  <updated>${ref}</updated>
+  <id>{url}/#{ref}</id>
+  <link href='{url}/#{ref}' />
+  <title>{ref}</title>
+  <updated>{ref}</updated>
   <author>
     <name>Henry Pr&#234;cheur</name>
     <email>henry@precheur.org</email>
   </author>
   <content type="xhtml">
-    <div xmlns="http://www.w3.org/1999/xhtml">\n''').\
-                         safe_substitute(url=url, ref=id_))
+    <div xmlns="http://www.w3.org/1999/xhtml">
+'''.format(url=url, ref=id_))
         x = xmlize(markdown(body))
-        sys.stdout.write(x.encode('utf8', 'xmlcharrefreplace'))
-        sys.stdout.write('    </div>\n  </content>\n</entry>\n')
-sys.stdout.write('</feed>')
+        out(x.encode('utf8', 'xmlcharrefreplace'))
+        out('    </div>\n  </content>\n</entry>\n')
+out('</feed>')
