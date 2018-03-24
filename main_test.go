3efd4a531031f238c8a8ef91db7048ae668ff5ce
Henry Precheur <henry@precheur.org>
2018-03-24 09:28:13 -0700

    fix tests

--- main_test.go
+++ main_test.go
@@ -77,8 +77,8 @@ func TestParsePost(t *testing.T) {
 	if err != nil {
 		t.Fatalf("bad error: %q", err)
 	}
-	var expected = time.Time{}
-	if p.Timestamp.Equal(expected) {
+	var expected = time.Date(2011, 12, 30, 21, 0, 0, 0, time.UTC)
+	if !p.Timestamp.Equal(expected) {
 		t.Fatalf("bad timestamp: %q != %q", p.Timestamp, expected)
 	}
 	if _, err = reader.ReadByte(); err != io.EOF {
@@ -86,21 +86,38 @@ func TestParsePost(t *testing.T) {
 	}
 }
 
-var exampleLog = ("2011-12-30T21:00:00Z\n\nLet's talk\n\f\n" +
-	"2011-12-31T21:00:00Z\n\nLet's talk\n\f\n")
-
 func TestParseLog(t *testing.T) {
-	var reader = bufio.NewReader(strings.NewReader(exampleLog))
-	var _, err = parseLog(reader)
+	var logText = ("2011-12-30T21:00:00Z\n\nLet's talk\n\f\n" +
+		"2011-12-31T21:00:00Z\n\nLet's talk\n\f\n")
+	var results = []Post{
+		{
+			Timestamp: time.Date(2011, 12, 30, 21, 00, 00, 0, time.UTC),
+			Body:      []byte("Let's talk\n"),
+		},
+		{
+			Timestamp: time.Date(2011, 12, 31, 21, 00, 00, 0, time.UTC),
+			Body:      []byte("Let's talk\n"),
+		},
+	}
+
+	var reader = bufio.NewReader(strings.NewReader(logText))
+	var log, err = parseLog(reader)
 	if err != io.EOF {
 		t.Fatalf("bad error: %q", err)
 	}
-	/*
-		var expected = time.Time{}
-		if p.Timestamp.Equal(expected) {
-			t.Fatalf("bad timestamp: %q != %q", p.Timestamp, expected)
+
+	for index, expected := range results {
+		var p = log[index]
+
+		if p.Timestamp != expected.Timestamp {
+			t.Fatalf("%q != %q", p.Timestamp, expected.Timestamp)
 		}
-	*/
+
+		if !bytes.Equal(p.Body, expected.Body) {
+			t.Fatalf("%q != %q", p.Body, expected.Body)
+		}
+	}
+
 	if _, err = reader.ReadByte(); err != io.EOF {
 		t.Fatalf("bad error at the end: %q != %q", err, io.EOF)
 	}
